ee692e44189dad32378b2d4df43d6be7a9463354
lynn Wang <1135464674@qq.com>
2022-03-14 19:37:11 +0800

    vidly-pagination&filter

---
+++ codeWithMosh/Front-end-Development/Libraries-and-Frameworks/MasteringReact/react-project/vidly/src/components/common/listGroup.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const ListGroup = (props) => {
+  const { items, textProperty, valueProperty, selectedItem, onItemSelected } =
+    props;
+
+    
+  return (
+    <ul className="list-group">
+      {items.map((item) => (
+        <li
+          key={item[valueProperty]}
+          className={
+            item === selectedItem ? "list-group-item active" : "list-group-item"
+          }
+          aria-current="true"
+          onClick={() => onItemSelected(item)}
+        >
+          {item[textProperty]}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+ListGroup.defaultProps = {
+  textProperty: "name",
+  valueProperty: "_id",
+};
+
+export default ListGroup;
